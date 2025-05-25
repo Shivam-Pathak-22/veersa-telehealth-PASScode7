1e868bcb9c33a1b7e3344377ecb62b15e2ad06bf
ShivanshiSingh96 <143714153+ShivanshiSingh96@users.noreply.github.com>
2025-05-25 22:26:56 +0530

    Create NewInterface.jsx

---
+++ Frontend/src/components/NewInterface/NewInterface.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from '../../pages/Dashboard/Dashboard.jsx';
+
+const NewInterface = () => {
+  return (
+    <div >
+      
+      <Dashboard />
+      
+    </div>
+  );
+};
+
+export default NewInterface;
